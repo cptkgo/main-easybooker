a60ea681533abe40de65e07b2e99cc7026491a75
cptkgo <info@online123>
2018-05-06 21:43:38 +0200

    base

---
+++ www/js/index.js
@@ -0,0 +1,57 @@
+cordova.plugins.barcodeScanner.scan(
+        function (result) {
+            if (!result.cancelled)
+            {
+                alert("Barcode type is: " + result.format);
+                alert("Decoded text is: " + result.text);
+            } else
+            {
+                alert("You have cancelled scan");
+            }
+        },
+        function (error) {
+            alert("Scanning failed: " + error);
+        }
+);
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+        
+        var notificationOpenedCallback = function(jsonData) {
+            console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+        };
+
+        window.plugins.OneSignal
+            .startInit("57af9006-12c3-4945-b39a-5198f3261caa")
+            .handleNotificationOpened(notificationOpenedCallback)
+         .endInit();
+    }, false);
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
